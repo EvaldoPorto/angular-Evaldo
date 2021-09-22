70b3bc75a941d265324c28b799ede1685eb13775
EvaldoPorto <evaldoporto@hotmail.com>
2021-09-22 17:43:19 -0300

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,6 +8,6 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent {
 
-  @Input() product!: Product | undefined;
+  @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
 }
